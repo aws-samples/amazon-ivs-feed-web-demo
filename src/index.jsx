495dc6b47573e1c9300f74a894c3adfd2ea31ac2
Andrei Dobre <andrei@fournine.digital>
2021-12-01 16:52:29 -0800

    sdk v1.6.1 update, package updates (#9)

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 import StreamProvider from './contexts/Stream/provider';
 import MobileBreakpointProvider from './contexts/MobileBreakpoint/provider';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import 'wicg-inert';
 import './index.css';
@@ -15,12 +15,10 @@ ReactDOM.render(
     <Router>
       <MobileBreakpointProvider>
         <StreamProvider>
-          <Route exact path="/">
-            <Redirect to="/0" />
-          </Route>
-          <Route path="/:id">
-            <App />
-          </Route>
+          <Routes>
+            <Route exact path="/" element={<Navigate to="/0" />}></Route>
+            <Route path="/:id" element={<App />}></Route>
+          </Routes>
         </StreamProvider>
       </MobileBreakpointProvider>
     </Router>
